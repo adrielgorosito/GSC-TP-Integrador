6c2209f2227021f2274d308a16f0c619bb1291f5
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-25 15:43:29 -0300

    Added Swagger

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,8 +1,5 @@
-using Backend.Controllers;
 using Backend.DataAccess;
-using Backend.DataAccess.Repositories;
 using Backend.DataAccess.UnitOfWork;
-using Backend.Domain;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,22 +9,12 @@ builder.Services.AddDbContext<LoanDbContext>(opt =>
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapControllers();
 app.Run();
-
-//using var context = new LoanDbContext();
-
-//LoansRepository loanRep = new LoansRepository(context);
-//Loan? l = await loanRep.GetOne(1);
-
-//// First database call test
-//Console.WriteLine("Id: " + l.Id + " - FechaPrest: " + l.Date + " - FechaEntr: " + l.ReturnDate);
-//Console.WriteLine("Persona: " + l.Person.Dni);
-//Console.WriteLine("Cosa: " + l.Thing.Description);
-
-//ThingsRepository thingRep = new ThingsRepository(context);
-//Thing? th = await thingRep.GetOne(2);
-//Console.WriteLine("Desc: " + th.Description + " - Categoría: " + th.Category.Description);
-
-// app.Run();
--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bafcbfb26833c831c4276c1a797670dc26b32647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24380a3f204cf964fb92ccde1165a76df27c34bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
