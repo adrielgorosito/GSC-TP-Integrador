26acfa7486e34efd5269fa9d87e8fed318f79eb3
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-25 18:44:26 -0300

    Data seeding

---
+++ Backend/Backend/DataAccess/DataSeeding.cs
@@ -0,0 +1,76 @@
+﻿using Backend.Domain;
+
+namespace Backend.DataAccess
+{
+    public class DataSeeding
+    {
+        public HashSet<Category> Categories;
+        public HashSet<Person> People;
+        public HashSet<dynamic> Things;
+        public HashSet<object> Loans;
+
+        public DataSeeding()
+        {
+            Categories = new HashSet<Category>
+            {
+                new Category() { Id = 1, Description = "Electronics" },
+                new Category() { Id = 2, Description = "Furniture" },
+                new Category() { Id = 3, Description = "Clothing" },
+                new Category() { Id = 4, Description = "Books" },
+                new Category() { Id = 5, Description = "Home Appliances" }
+            };
+
+            People = new HashSet<Person>
+            {
+                new Person()
+                {
+                    Dni = 42125750,
+                    Name = "Adriel",
+                    PhoneNumber = "3476123456",
+                    EmailAddress = "adrielgorosito14@gmail.com"
+                },
+                new Person()
+                {
+                    Dni = 42000000,
+                    Name = "Beto",
+                    PhoneNumber = "341123456",
+                    EmailAddress = "betorc@gsc.com"
+                },
+                new Person()
+                {
+                    Dni = 42123456,
+                    Name = "Walter",
+                    PhoneNumber = "341654321",
+                    EmailAddress = "walter@gsc.com"
+                }
+            };
+
+            Things = new HashSet<object>
+            {
+                new { Id = 1, Description = "Notebook", CategoryId = Categories.First(c => c.Id == 1).Id },
+                new { Id = 2, Description = "Smartphone", CategoryId = Categories.First(c => c.Id == 1).Id },
+                new { Id = 3, Description = "Sofa", CategoryId = Categories.First(c => c.Id == 2).Id },
+                new { Id = 4, Description = "T-shirt", CategoryId = Categories.First(c => c.Id == 3).Id },
+                new { Id = 5, Description = "Jeans", CategoryId = Categories.First(c => c.Id == 3).Id },
+                new { Id = 6, Description = "Sweater", CategoryId = Categories.First(c => c.Id == 3).Id },
+                new { Id = 7, Description = "Socks", CategoryId = Categories.First(c => c.Id == 3).Id },
+                new { Id = 8, Description = "Shorts", CategoryId = Categories.First(c => c.Id == 3).Id },
+                new { Id = 9, Description = "Speaker", CategoryId = Categories.First(c => c.Id == 5).Id }
+            };
+
+            Loans = new HashSet<dynamic>
+            {
+                new { Id = 1, Date = new DateOnly(2023, 12, 1), ReturnDate = new DateOnly(2023, 12, 5), PersonDni = People.First(p => p.Name == "Adriel").Dni, ThingId = Things.First(t => t.Id == 1).Id },
+                new { Id = 2, Date = new DateOnly(2023, 12, 2), PersonDni = People.First(p => p.Name == "Beto").Dni, ThingId = Things.First(t => t.Id == 2).Id },
+                new { Id = 3, Date = new DateOnly(2023, 12, 3), ReturnDate = new DateOnly(2023, 12, 7), PersonDni = People.First(p => p.Name == "Walter").Dni, ThingId = Things.First(t => t.Id == 3).Id },
+                new { Id = 4, Date = new DateOnly(2023, 12, 4), PersonDni = People.First(p => p.Name == "Adriel").Dni, ThingId = Things.First(t => t.Id == 4).Id },
+                new { Id = 5, Date = new DateOnly(2023, 12, 5), PersonDni = People.First(p => p.Name == "Beto").Dni, ThingId = Things.First(t => t.Id == 5).Id },
+                new { Id = 6, Date = new DateOnly(2023, 12, 6), PersonDni = People.First(p => p.Name == "Walter").Dni, ThingId = Things.First(t => t.Id == 6).Id },
+                new { Id = 7, Date = new DateOnly(2023, 12, 7), ReturnDate = new DateOnly(2023, 12, 11), PersonDni = People.First(p => p.Name == "Adriel").Dni, ThingId = Things.First(t => t.Id == 7).Id },
+                new { Id = 8, Date = new DateOnly(2023, 12, 8), PersonDni = People.First(p => p.Name == "Beto").Dni, ThingId = Things.First(t => t.Id == 8).Id },
+                new { Id = 9, Date = new DateOnly(2023, 12, 9), ReturnDate = new DateOnly(2023, 12, 13), PersonDni = People.First(p => p.Name == "Walter").Dni, ThingId = Things.First(t => t.Id == 9).Id },
+                new { Id = 10, Date = new DateOnly(2023, 12, 10), PersonDni = People.First(p => p.Name == "Adriel").Dni, ThingId = Things.First(t => t.Id == 2).Id },
+            };
+        }
+    }
+}
--- Backend/Backend/DataAccess/LoanDbContext.cs
+++ Backend/Backend/DataAccess/LoanDbContext.cs
@@ -20,24 +20,30 @@ namespace Backend.DataAccess
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            DataSeeding DataSeeding = new DataSeeding();
+
             modelBuilder.Entity<Person>(p =>
             {
                 p.HasKey(per => per.Dni);
                 p.Property(per => per.Dni).ValueGeneratedNever();
+                p.HasData(DataSeeding.People);
             });
 
             modelBuilder.Entity<Category>(c =>
             {
                 c.HasIndex(cat => cat.Description).IsUnique();
                 c.Property(cat => cat.CreationDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                c.HasData(DataSeeding.Categories);
             });
 
             modelBuilder.Entity<Thing>(t =>
             {
                 t.HasIndex(th => th.Description).IsUnique();
                 t.Property(th => th.CreationDate).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                t.HasData(DataSeeding.Things);
             });
-                
+
+            modelBuilder.Entity<Loan>(l => l.HasData(DataSeeding.Loans));
         }
     }
 }
