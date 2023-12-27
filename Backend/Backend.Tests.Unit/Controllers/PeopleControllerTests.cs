b93ae53c1bfa548e4db60535d0fe681e6e89a185
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-27 17:11:31 -0300

    Fix error

--- Backend/Backend.Tests.Unit/Controllers/PeopleControllerTests.cs
+++ Backend/Backend.Tests.Unit/Controllers/PeopleControllerTests.cs
@@ -20,7 +20,7 @@ namespace Backend.Tests.Unit.Controllers
         public PeopleControllerTests()
         {
             Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Testing");
-            
+
             this.connection.Open();
             var options = new DbContextOptionsBuilder().UseSqlite(this.connection).Options;
             this.context = new LoanDbContext(options);
@@ -35,7 +35,7 @@ namespace Backend.Tests.Unit.Controllers
             await this.context.Database.EnsureCreatedAsync();
 
             await this.context.AddRangeAsync(
-                new Person() { Dni = 1, Name = "Adriel", PhoneNumber = "123", EmailAddress = "adriel@g.com"},
+                new Person() { Dni = 1, Name = "Adriel", PhoneNumber = "123", EmailAddress = "adriel@g.com" },
                 new Person() { Dni = 2, Name = "Walter", PhoneNumber = "111", EmailAddress = "walter@g.com" },
                 new Person() { Dni = 3, Name = "Beto", PhoneNumber = "321", EmailAddress = "betorc@g.com" }
             );
@@ -79,7 +79,7 @@ namespace Backend.Tests.Unit.Controllers
                 ActionResult<Person>? actual = await this.sut.GetPersonByDni(100);
 
                 // assert
-                actual.Result.Should().BeOfType<NotFoundResult>();
+                actual!.Result.Should().BeOfType<NotFoundResult>();
                 actual.Value.Should().BeNull();
             }
         }
@@ -189,108 +189,5 @@ namespace Backend.Tests.Unit.Controllers
                 actual.Should().BeOfType<NotFoundResult>();
             }
         }
-        public class TheMethod_AddPerson : PeopleControllerTests
-        {
-            [Fact]
-            public async Task Should_add_person_successfully()
-            {
-                // arrange
-                await this.InitAsync();
-                var newPerson = new Person() { Dni = 4, Name = "Luciano", PhoneNumber = "555", EmailAddress = "diamand@g.com" };
-
-                // act
-                ActionResult actual = await this.sut.AddPerson(newPerson);
-
-                // assert
-                actual.Should().BeOfType<NoContentResult>();
-
-                this.Dispose();
-            }
-
-            [Fact]
-            public async Task Should_return_error_BadRequest_when_dni_already_exists()
-            {
-                // arrange
-                await this.InitAsync();
-                var duplicatePerson = new Person() { Dni = 1, Name = "GSC", PhoneNumber = "999", EmailAddress = "gsc@g.com" };
-
-                // act
-                ActionResult actual = await this.sut.AddPerson(duplicatePerson);
-
-                // assert
-                actual.Should().BeOfType<BadRequestObjectResult>();
-
-                this.Dispose();
-            }
-        }
-
-        public class TheMethod_UpdatePerson : PeopleControllerTests
-        {
-            [Fact]
-            public async Task Should_update_person_successfully()
-            {
-                // arrange
-                await this.InitAsync();
-                var updatedPerson = new Person() { Dni = 1, Name = "Gorosito", PhoneNumber = "777", EmailAddress = "gorosito@g.com" };
-
-                // act
-                ActionResult actual = await this.sut.UpdatePerson(updatedPerson);
-
-                // assert
-                actual.Should().BeOfType<NoContentResult>();
-
-                this.Dispose();
-            }
-
-            [Fact]
-            public async Task Should_return_NotFound_when_person_does_not_exists()
-            {
-                // arrange
-                await this.InitAsync();
-                var nonExistingPerson = new Person() { Dni = 100, Name = "NonExistingPerson", PhoneNumber = "000", EmailAddress = "error@g.com" };
-
-                // act
-                ActionResult actual = await this.sut.UpdatePerson(nonExistingPerson);
-
-                // assert
-                actual.Should().BeOfType<NotFoundResult>();
-
-                this.Dispose();
-            }
-        }
-
-        public class TheMethod_DeletePerson : PeopleControllerTests
-        {
-            [Fact]
-            public async Task Should_delete_person_successfully()
-            {
-                // arrange
-                await this.InitAsync();
-
-                // act
-                ActionResult actual = await this.sut.DeletePerson(1);
-
-                // assert
-                actual.Should().BeOfType<NoContentResult>();
-
-                this.Dispose();
-            }
-
-            [Fact]
-            public async Task Should_return_NotFound_when_person_does_not_exists()
-            {
-                // arrange
-                await this.InitAsync();
-
-                // act
-                ActionResult actual = await this.sut.DeletePerson(100);
-
-                // assert
-                actual.Should().BeOfType<NotFoundResult>();
-
-                this.Dispose();
-            }
-        }
-
     }
 }
