81f5b717e27cdd072bc5a4aaf692a8ac289ab991
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-28 20:53:59 -0300

    Error and people-crud styles

--- Frontend/src/app/people-crud/people-crud.component.ts
+++ Frontend/src/app/people-crud/people-crud.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-people-crud',
   templateUrl: './people-crud.component.html',
-  styleUrls: ['./people-crud.component.css']
+  styleUrls: ['./people-crud.component.css'],
 })
 export class PeopleCRUDComponent {
+  people: Person[] = [];
 
+  ngOnInit(): void {
+    // Aquí puedes inicializar 'people' con datos iniciales o llamar a un servicio para obtener los datos
+    this.initializePeople();
+  }
+
+  initializePeople() {
+    this.people = [
+      new Person(123456789, 'John Doe', '555-1234', 'john@example.com'),
+      new Person(987654321, 'Jane Smith', '555-5678', 'jane@example.com'),
+    ];
+  }
+
+  deletePerson(_t18: any) {
+    throw new Error('Method not implemented.');
+  }
+  updatePerson(_t18: any) {
+    throw new Error('Method not implemented.');
+  }
 }
