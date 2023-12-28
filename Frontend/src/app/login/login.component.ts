1f630ceaa5bbaea6434f02987ea7f7fa35abe67d
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-28 11:59:27 -0300

    First frontend commit: method getAllPeople on console

---
+++ Frontend/src/app/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { User } from '../user';
+import { UserService } from '../user.service';
+import { PersonService } from '../person.service';
+import { Person } from '../person';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  constructor(
+    private fb: FormBuilder,
+    private us: UserService,
+    private ps: PersonService
+  ) {}
+  // borrar personService, eso se haría en otro .ts
+
+  error: boolean = false;
+
+  loginForm = this.fb.group({
+    user: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  submit() {
+    if (!this.loginForm.invalid) {
+      const u = new User(
+        this.loginForm.get('user')!.value!,
+        this.loginForm.get('password')!.value!
+      );
+
+      const observer = {
+        next: (people: Person[]) => {
+          console.log('Personas obtenidas:', people);
+        },
+        error: (obsError: string) => {
+          console.error('Error al obtener personas:', obsError);
+        },
+      };
+
+      this.ps.getAll().subscribe(observer);
+    }
+  }
+}
---
+++ Frontend/src/app/person.ts
@@ -0,0 +1,8 @@
+export class Person {
+  constructor(
+    public dni: number,
+    public uname: string,
+    public phoneNumber: string,
+    public emailAddress: string
+  ) {}
+}
