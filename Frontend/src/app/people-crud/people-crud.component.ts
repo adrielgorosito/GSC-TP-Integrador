57c3fd3fe5e0927e090d76a58274313fdd91b654
Adriel Gorosito <adrielgorosito14@gmail.com>
2023-12-29 00:40:11 -0300

    Added HTTP methods

--- Frontend/src/app/people-crud/people-crud.component.ts
+++ Frontend/src/app/people-crud/people-crud.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Person } from '../person';
 import { PersonService } from '../person.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-people-crud',
@@ -8,12 +9,19 @@ import { PersonService } from '../person.service';
   styleUrls: ['./people-crud.component.css'],
 })
 export class PeopleCRUDComponent {
-  constructor(private ps: PersonService) {}
+  constructor(private ps: PersonService, private router: Router) {}
 
   people: Person[] = [];
   dataLoaded: boolean = false;
 
   ngOnInit(): void {
+    this.getAllPeople();
+  }
+
+  protected getAllPeople() {
+    if (localStorage.getItem('token') == null)
+      this.router.navigate(['/error-crud']);
+
     const observer = {
       next: (data: Person[]) => {
         this.people = data;
@@ -26,13 +34,66 @@ export class PeopleCRUDComponent {
     this.ps.getAllPeople().subscribe(observer);
   }
 
-  deletePerson(person: Person) {
-    throw new Error('Method not implemented.');
+  protected getPersonByDni(dni: number) {
+    if (localStorage.getItem('token') == null)
+      this.router.navigate(['/error-crud']);
+
+    const observer = {
+      next: (data: Person) => {
+        // apply logic
+      },
+      error: (error: any) => {
+        console.error('Error fetching person by DNI:', error);
+      },
+    };
+
+    this.ps.getPersonByDni(dni).subscribe(observer);
   }
-  updatePerson(person: Person) {
-    throw new Error('Method not implemented.');
+
+  protected addPerson(person: Person) {
+    // if (localStorage.getItem('token') == null)
+    //   this.router.navigate(['/error-crud']);
+    // const observer = {
+    //   next: (data: Person) => {
+    //     // ???
+    //     const addedPerson: Person = data;
+    //   },
+    //   error: (error: any) => {
+    //     console.error('Error adding person:', error);
+    //   },
+    // };
+    // this.ps.addPerson(person).subscribe(observer);
+    //
+    /* maybe I can use: <router-outlet></router-outlet>*/
   }
-  addPerson() {
-    throw new Error('Method not implemented.');
+
+  protected updatePerson(person: Person) {
+    if (localStorage.getItem('token') == null)
+      this.router.navigate(['/error-crud']);
+
+    const observer = {
+      next: (data: Person) => {
+        // apply logic
+        const updatedPerson: Person = data;
+      },
+      error: (error: any) => {
+        console.error('Error updating person:', error);
+      },
+    };
+  }
+
+  protected deletePerson(person: Person) {
+    if (localStorage.getItem('token') == null)
+      this.router.navigate(['/error-crud']);
+
+    const observer = {
+      next: (data: Person) => {
+        // apply logic
+        const deletedPerson: Person = data;
+      },
+      error: (error: any) => {
+        console.error('Error deleting person:', error);
+      },
+    };
   }
 }
--- Frontend/src/app/person.service.ts
+++ Frontend/src/app/person.service.ts
@@ -3,26 +3,51 @@ import { Person } from './person';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, switchMap, throwError } from 'rxjs';
 import { UserService } from './user.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PersonService {
-  constructor(private http: HttpClient, private us: UserService) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   private apiPeople = 'http://localhost:5000/api/people';
 
   public getAllPeople(): Observable<Person[]> {
+    const headers = this.headerToken();
+    return this.http.get<Person[]>(this.apiPeople, { headers });
+  }
+
+  public getPersonByDni(dni: number): Observable<Person> {
+    const headers = this.headerToken();
+    return this.http.get<Person>(`${this.apiPeople}/${dni}`, { headers });
+  }
+
+  public addPerson(person: Person): Observable<Person> {
+    const headers = this.headerToken();
+    return this.http.post<Person>(this.apiPeople, person, { headers });
+  }
+
+  public deletePerson(dni: number): Observable<Person> {
+    const headers = this.headerToken();
+    return this.http.delete<Person>(`${this.apiPeople}/${dni}`, { headers });
+  }
+
+  public updatePerson(person: Person): Observable<Person> {
+    const headers = this.headerToken();
+    return this.http.put<Person>(this.apiPeople, person, { headers });
+  }
+
+  private headerToken(): HttpHeaders {
     const token = localStorage.getItem('token');
 
     if (!token) {
-      return throwError(() => 'Token not available');
+      this.router.navigate(['/error-crud']);
+      throw new Error('Token not available');
     }
 
-    const headers = new HttpHeaders({
+    return new HttpHeaders({
       Authorization: `Bearer ${token}`,
     });
-
-    return this.http.get<Person[]>(this.apiPeople, { headers });
   }
 }
